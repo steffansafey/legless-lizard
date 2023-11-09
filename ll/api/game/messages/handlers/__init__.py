31f85cccf73dcc0c7395ca4039a66fe97ce29bf8
Tim Leathart <tim@sportsflare.io>
2023-11-09 16:02:08 +1300

    Add client_update message and take_step logic

--- ll/api/game/messages/handlers/__init__.py
+++ ll/api/game/messages/handlers/__init__.py
@@ -1,1 +1,1 @@
-from .client import handle_join_request  # noqa: F401
+from .client import handle_client_update, handle_join_request  # noqa: F401
--- ll/api/game/messages/handlers/client.py
+++ ll/api/game/messages/handlers/client.py
@@ -1,9 +1,10 @@
 import uuid
+from random import randint
 
 from structlog import get_logger
 
-from ll.api.game.messages.resources import JoinRequest, JoinResponse
-from ll.api.game.resources import GamePlayer
+from ll.api.game.messages.resources import ClientUpdate, JoinRequest, JoinResponse
+from ll.api.game.resources import GamePlayer, GameState, PlayerStep
 
 logger = get_logger(__name__)
 
@@ -36,14 +37,41 @@ async def handle_join_request(request, message_wrapper: JoinRequest):
         id=str(uuid.uuid4()),
         name=message_wrapper.name,
         color=message_wrapper.color,
-        step_length=1,
-        position=[0, 0],
-        direction=[0, 1],
+        step_length=50,
         spawned=False,
-        steps=[],
+        steps=[
+            PlayerStep(
+                coordinates=[
+                    float(randint(0, 100)),
+                    float(randint(0, 100)),
+                ]
+            )
+        ],
     )
     app["game_states"][1].players.append(player)
     logger.info("player joined", existing_player=False)
     await assign_player_id_to_ws_connection(app, player.id, request.conn)
 
     return [JoinResponse(player_id=player.id, ok=True)]
+
+
+async def handle_client_update(request, message_wrapper: ClientUpdate):
+    """Handle a client update."""
+    app = request.app
+    logger.info("handle_client_update", message_wrapper=message_wrapper)
+
+    game_state: GameState = app["game_states"][1]
+
+    # check if the player exists (lookup by ID)
+    # TODO: Get this from session somehow
+    player: GamePlayer = next(
+        (p for p in game_state.players if p.id == message_wrapper.player_id),
+        None,
+    )
+    if not player:
+        logger.warning("player not found", player_id=message_wrapper.player_id)
+        return []
+
+    # update player angle
+    player.angle = message_wrapper.angle
+    return []
